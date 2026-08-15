a3254fcc1d2649ab0371e47a076753483421bcd1
Krish <krish@Mac.Home>
2026-08-15 22:11:29 +0100

    Opted to make a Custom Logger class to always attach JSON formatter to any logger instances. Ensured validation of the log-level that is passed from environment into the logger on instantiation

--- src/phishing_detector/config/logger.py
+++ src/phishing_detector/config/logger.py
@@ -2,6 +2,7 @@ import logging
 import json
 from datetime import datetime, UTC
 import sys
+from enum import IntEnum
 
 """
 The hierachy for python logging is: DEBUG, INFO, WARNING, ERROR, CRITICAL
@@ -12,13 +13,21 @@ There are also components relevant to logging:
 - Formatter: How the log actually looks
 """
 
-# Set the minimum level of logging
-logging.basicConfig(level=logging.INFO)
+# Defined an enum to map the environment variables to the integer representations of log valuess expected when new Logger is created
+class LogLevel(IntEnum):
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+    ERROR = logging.ERROR
+    CRITICAL = logging.CRITICAL
 
-"""
-Create a custom formatter, which inherits from the base logging.Formatter
-"""
+
+
 class JsonFormatter(logging.Formatter):
+    """
+    Create a custom formatter, which inherits from the base logging.Formatter, and override the format method to
+    format messages into a custom json format, to make logs easily searchible
+    """
     def format(self, record: logging.LogRecord) -> str:
         # We can define a custom structure for the format
         payload = {
@@ -31,21 +40,21 @@ class JsonFormatter(logging.Formatter):
         return json.dumps(payload)
 
 
-logger = logging.getLogger(name="phishing_detector")
 
+class CustomLogger(logging.Logger):
+    """
+    Create a CustomLogger object, that inherits from logger. On initialisation, requires a name and log_level to be 
+    specified and automatically configures its handler to use the custom formatter. 
+    """
+    def __init__(self, name: str, level : LogLevel | str = LogLevel.INFO):
+        super().__init__(name, level)
 
-"""
-Attach the Formatter to a Handler
-"""
-handler = logging.StreamHandler(sys.stdout) # Emit logs and let runtime collect them, Will also use docker downstream
-handler.setFormatter(fmt=JsonFormatter())
+        # Initate an instance of the handler
+        handler = logging.StreamHandler(sys.stdout)
+        # Attach an instance of the formatter to the handler
+        handler.setFormatter(JsonFormatter())
 
-"""
-Attach the Handler to the Logger
-"""
-logger.addHandler(handler)
-
-logger.info("My very new log that should be formatted")
-
+        # Attach the handler to the logger
+        self.addHandler(handler)
 
 
