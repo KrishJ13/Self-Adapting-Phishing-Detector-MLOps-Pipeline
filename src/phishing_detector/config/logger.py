0c04ff2b2f0063c6cb1d67940090ee3e05cbeca7
Krish <krish@Mac.Home>
2026-08-15 17:55:30 +0100

    Added custom JsonFormatter for structured and machine-queryable logging.

--- src/phishing_detector/config/logger.py
+++ src/phishing_detector/config/logger.py
@@ -1,4 +1,7 @@
 import logging
+import json
+from datetime import datetime, UTC
+import sys
 
 """
 The hierachy for python logging is: DEBUG, INFO, WARNING, ERROR, CRITICAL
@@ -9,7 +12,40 @@ There are also components relevant to logging:
 - Formatter: How the log actually looks
 """
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+# Set the minimum level of logging
+logging.basicConfig(level=logging.INFO)
+
+"""
+Create a custom formatter, which inherits from the base logging.Formatter
+"""
+class JsonFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        # We can define a custom structure for the format
+        payload = {
+            "timestamp": datetime.fromtimestamp(record.created, tz=UTC).isoformat(), #  record.created is the actual UNIX timestamp the log was created, not when it ran
+            "level": record.levelname,
+            "logger": record.name,
+            "event": record.getMessage()
+        }
+        # Payload is in Json, we need to return to string
+        return json.dumps(payload)
+
+
 logger = logging.getLogger(name="phishing_detector")
-logger.info("Application started")
+
+
+"""
+Attach the Formatter to a Handler
+"""
+handler = logging.StreamHandler(sys.stdout) # Emit logs and let runtime collect them, Will also use docker downstream
+handler.setFormatter(fmt=JsonFormatter())
+
+"""
+Attach the Handler to the Logger
+"""
+logger.addHandler(handler)
+
+logger.info("My very new log that should be formatted")
+
+
 
